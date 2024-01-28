028dd02f407945a324d92d3de40bd0ec9b62b067
LampTales <179500516@qq.com>
2024-01-28 23:39:21 +0800

    recognize debug

--- src/recognize.py
+++ src/recognize.py
@@ -5,9 +5,9 @@ def recognize(model, filename):
     file = os.path.join(os.path.dirname(__file__), filename)
     print(file)
     result = model.transcribe(file)
-    return result.text
+    return result
 
 if __name__ == '__main__':
-    model = whisper.load_model(name='tiny')
+    model = whisper.load_model(name='tiny', device='cpu')
     result = recognize(model, 'test.wav')
-    print(result)
+    print(result.get('text'))
