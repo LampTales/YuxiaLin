175f7e9947189faf8f4671dca58e87f53eece2df
LampTales <179500516@qq.com>
2024-02-04 02:42:23 +0800

    client test

--- src/client.py
+++ src/client.py
@@ -1,8 +1,13 @@
 import http.client
 import os
 import argparse
+from playsound import playsound
+
 from urllib import parse as urlparse
 from record import get_word
+
+
+conn = None
 
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -10,8 +15,43 @@ def arg_parser():
     parser.add_argument('--port', '-p', help='port to connect', type=int, default=8081)
     return parser.parse_args()
 
+def activate():
+    return True
+
+def rec_rep_tts():
+    pass
+
+def go_on():
+    pass
+
+
+def run_client(host, port):
+    
+
+    while True:
+        global conn
+        conn = http.client.HTTPConnection(host, port)
+
+        while not activate():
+            continue
+
+        get_word()
+
+        while True:
+            rec_rep_tts()
+
+            if not go_on():
+                break
+
+
+
+
+        
+    
+
 def main():
     wav_path = 'test.wav'
+
     get_word(filename=wav_path)
 
     args = arg_parser()
@@ -22,7 +62,7 @@ def main():
     with open(wav_path, 'rb') as f:
         data = f.read()
         length = len(data)
-    conn.request('POST', '/rec', headers={'Operation': 'rec', 'Content-Length': length})
+    conn.request('POST', '/rep', headers={'Operation': 'rep', 'Content-Length': length})
     conn.send(data)
     response = conn.getresponse()
     print(response.status, response.reason)
@@ -36,5 +76,16 @@ def main():
     # end the connection
     conn.close()
 
+    # play the wav
+    playsound('./receive_from_server.wav')
+
+
+def play_test():
+    # playsound('../lin_voice/zh/信赖触摸.wav')
+    playsound('receive_from_server.wav')
+
+
+
 if __name__ == '__main__':
     main()
+    # play_test()
