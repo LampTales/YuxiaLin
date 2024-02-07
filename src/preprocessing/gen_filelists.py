d9188a6d671e559488ed0f11a3eb449fbb2f11e2
LampTales <179500516@qq.com>
2024-02-08 00:28:27 +0800

    gen_filelists build

--- src/preprocessing/gen_filelists.py
+++ src/preprocessing/gen_filelists.py
@@ -1,9 +1,11 @@
 import os
 import whisper
 
-file_list_path = 'filelists/lin.list'
+data_name = 'lin'
 
-raw_data_path = 'raw/'
+file_list_path = 'filelists/' + data_name + '.list'
+
+raw_data_path = 'raw/' + data_name + '/'
 
 model_size = 'medium'
 model = whisper.load_model(name=model_size)
@@ -11,3 +13,11 @@ model = whisper.load_model(name=model_size)
 def gen_list(file_list_path, raw_data_path):
     file_list = open(file_list_path, 'w')
     
+    # deal with all the dirs in raw_data_path
+    for dir in os.listdir(raw_data_path):
+        lang = dir.split('_')[0]
+        speaker = data_name + '_' + lang
+        for file in os.listdir(raw_data_path + dir):
+            text = model.transcribe(raw_data_path + dir + '/' + file)
+            entry = f'./dataset/{data_name}/{speaker}/{file}|{speaker}|{lang.upper()}|{text}\n'
+            file_list.write(entry)
